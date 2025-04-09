8c4613ddb5e83925b7e8c0415b1ad703f0ad3faf
AlexXeno <arowe@xenomorph.com>
2025-04-09 16:11:26 +0200

    Update DeployRelease.bat

--- DeploymentScripts/DeployRelease.bat
+++ DeploymentScripts/DeployRelease.bat
@@ -1,6 +1,7 @@
 ECHO Running deployment script...
-powershell "& '%~dp0\ApplyDBT.ps1'" > "%~dp0\Logs\ApplyDBT_xENVx.log"
-powershell "& '%~dp0\ImportXDBItems.ps1'" > "%~dp0\Logs\ImportXDBItems_xENVx.log"
-powershell "& '%~dp0\CopyWorkflowScripts.ps1'" > "%~dp0\Logs\CopyWorkflowScripts_xENVx.log"
-powershell "& '%~dp0\RunSqlScripts.ps1'" > "%~dp0\Logs\RunSqlScripts_xENVx.log"
+mkdir "%~dp0\Logs"
+powershell "& '%~dp0\ApplyDBT.ps1'" > "%~dp0\Logs\ApplyDBT.log"
+powershell "& '%~dp0\ImportXDBItems.ps1'" > "%~dp0\Logs\ImportXDBItems.log"
+powershell "& '%~dp0\CopyWorkflowScripts.ps1'" > "%~dp0\Logs\CopyWorkflowScripts.log"
+powershell "& '%~dp0\RunSqlScripts.ps1'" > "%~dp0\Logs\RunSqlScripts.log"
 ECHO deployment completed...
